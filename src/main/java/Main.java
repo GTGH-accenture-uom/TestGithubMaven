996eb1dcd21cecf8d0055f514c2a0463d82c24e3
Nikos Nikolaidis <nikos.gate4.nik@gmail.com>
2022-05-27 12:29:37 +0300

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            String data = FileUtils.readFileToString(
+                    new File("C:/Users/Nikos/Desktop/commons-io/TestMaven/src/main/resources/file.txt"),
+                    "UTF-8");
+
+            System.out.println("data: " + data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //Log4j
+        Logger logger = LogManager.getLogger(Main.class);
+        logger.info("This is an info");
+        logger.error("This is an error");
+    }
+
+}
