6f353c2eac36092f919a3d67c6e3b588b2f3fd80
Nikos Nikolaidis <nikos.gate4.nik@gmail.com>
2022-05-27 12:51:28 +0300

    change2 README.md

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main {
             e.printStackTrace();
         }
 
-
+        System.out.println("dd");
         //Log4j
         Logger logger = LogManager.getLogger(Main.class);
         logger.info("This is an info");
